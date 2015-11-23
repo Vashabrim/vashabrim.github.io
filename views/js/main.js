5f191ae216211b5a0d52d1f50f432d1230339877
Vashabrim <vashabrim@gmail.com>
2015-11-22 22:23:06 -0600

    hh

--- views/js/main.js
+++ views/js/main.js
@@ -502,11 +502,12 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
+    //var items = document.querySelectorAll('.mover');
+  var items = document.getElementsByClassName('.mover');
   for (var i = 0; i < items.length; i++) {
       var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-      //items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
-      items[i].style["-webkit-transform"] = "translate(xPos, yPos)";
+      items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+      //items[i].style["-webkit-transform"] = "translate(xPos, yPos)";
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
