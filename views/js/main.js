9b9d4edcffc58c7d2edc68268e880fc3c14fc87c
Vashabrim <vashabrim@gmail.com>
2015-11-22 22:10:34 -0600

    js

--- views/js/main.js
+++ views/js/main.js
@@ -506,7 +506,7 @@ function updatePositions() {
   for (var i = 0; i < items.length; i++) {
       var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
       //items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
-      items.[i].style["-webkit-transform"] = "translate(xPos, yPos)";
+      items[i].style["-webkit-transform"] = "translate(xPos, yPos)";
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
